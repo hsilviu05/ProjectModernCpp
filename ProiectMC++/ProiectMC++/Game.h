f88b3038b731c2da141f3913f83934633e131abd
Silviu-Unitbv <ionut.hermeneanu@student.unitbv.ro>
2024-11-03 19:19:10 +0200

    Game.h header *

---
+++ ProiectMC++/ProiectMC++/Game.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "Bullet.h"
+#include "Player.h"
+#include "Map.h"
+#include "Wall.h"
+
+class Game
+{
+public:
+	Game();
+	void start();
+	void update();
+	void handleInput();
+	void render();
+
+private:
+	Map m_map;
+	std::vector<Bullet> m_bullets;
+	std::vector<Wall> m_walls;
+
+};
