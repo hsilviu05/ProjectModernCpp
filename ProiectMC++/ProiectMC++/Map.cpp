6f91c2c6ff7a88d4e43272f42a73b3cd3ad65783
Iftime_Razvan <razvan.iftime@student.unitbv.ro>
2024-11-13 23:19:22 +0200

    Add generateMap and allocation function for it

--- ProiectMC++/ProiectMC++/Map.cpp
+++ ProiectMC++/ProiectMC++/Map.cpp
@@ -1,21 +1,42 @@
 #include "Map.h"
 
 
-uint16_t Map::getHeight() const{
+void Map::Allocation(){
+	m_gameArea.resize(m_height);
+	for (int i = 0; i < m_height; i++)
+		m_gameArea[i].resize(m_width);
+}
+
+size_t Map::getHeight() const{
 	return m_height;
 }
 
-void Map::setHeight(uint16_t height){
-	m_height = height;
+void Map::GenerateMap(){
+
+	std::random_device rd;
+	std::mt19937 gen(rd());
+
+	std::uniform_int_distribution<size_t> dist_size(12, 25);
+	m_height = dist_size(gen);
+	m_width = dist_size(gen);
+
+	std::uniform_int_distribution<uint16_t> dist_tile(0, 2);
+	Allocation();
+	for(int i=0;i<m_height;i++){
+		for (int j = 0; j < m_width; j++)
+			if ((i == 0 || i == m_height) && (j == 0 || j == m_width))
+				m_gameArea[i][j] = TileType::EmptySpace;
+			else
+				m_gameArea[i][j] = static_cast<TileType>(dist_tile(gen));
+	}
+	
+
 }
 
-uint16_t Map::getWidth() const{
+size_t Map::getWidth() const{
 	return m_width;
 }
 
-void Map::setWidth(uint16_t width){
-	m_width = width;
-}
 
 TileType Map::getTile(int x, int y) const
 {
@@ -34,13 +55,12 @@ void Map::destroyTile(int x, int y)
 	}
 }
 
-void Map::draw() const
+void Map::Draw() const
 {
 	for (const auto& row : m_gameArea) {
-		for (const auto& tile : row)
-		{
-			std::cout << static_cast<int>(tile) << ' ';
+		for (const auto& tile : row){
+			std::cout << static_cast<int>(tile) << " ";
 		}
-		std::cout << '\n';
+		std::cout << "\n";
 	}
 }
--- ProiectMC++/ProiectMC++/Map.h
+++ ProiectMC++/ProiectMC++/Map.h
@@ -4,20 +4,21 @@
 #include<array>
 #include<cstdint>
 #include<iostream>
+#include<random>
 
 class Map
 {
 	private:
 		std::vector<std::vector<TileType>>m_gameArea;
-		uint16_t m_height;
-		uint16_t m_width;
-		std::array<std::pair<uint16_t, uint16_t>,4>m_startPositions;
+		size_t m_height;
+		size_t m_width;
+		void Allocation();
+		std::array<std::pair<size_t, size_t>,4>m_startPositions;
 	public:
-		uint16_t getHeight() const;
-		void setHeight(uint16_t height);
-		uint16_t getWidth() const;
-		void setWidth(uint16_t width);
+		size_t getHeight() const;
+		void GenerateMap();
+		size_t getWidth() const;
 		TileType getTile(int x, int y) const;
 		void destroyTile(int x, int y);
-		void draw() const;
+		void Draw() const;
 };
