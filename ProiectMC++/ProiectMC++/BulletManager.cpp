65e9e012b96722987faa23320352294346895a95
Silviu-Unitbv <ionut.hermeneanu@student.unitbv.ro>
2024-12-12 22:23:50 +0200

    Redo Collisions Check

--- ProiectMC++/ProiectMC++/BulletManager.cpp
+++ ProiectMC++/ProiectMC++/BulletManager.cpp
@@ -52,6 +52,48 @@ void BulletManager::CheckBulletWallCollisions(const std::vector<Wall>& walls, Ma
     }
 }
 
+void BulletManager::CheckBulletBulletCollisions() {
+    for (auto it1 = m_bullets.begin(); it1 != m_bullets.end(); ++it1) {
+        for (auto& bullet1 : it1->second) {
+            if (bullet1.IsActive()) {
+                for (auto it2 = m_bullets.begin(); it2 != m_bullets.end(); ++it2) {
+                    for (auto& bullet2 : it2->second) {
+                        if (&bullet1 != &bullet2 && bullet2.IsActive() && bullet1.getPosition() == bullet2.getPosition()) {
+                            bullet1.DeactivateBullet();
+                            bullet2.DeactivateBullet();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+void BulletManager::CheckBulletPlayersCollisions(std::array<Player, 4>& players) {
+    for (auto& [shooterID, bullets] : m_bullets) {
+        for (auto& bullet : bullets) {
+            if (bullet.IsActive()) {
+                for (auto& player : players) {
+                    if (bullet.getPosition() == player.getPosition()) {
+                        if (shooterID != player.GetPlayerID()) {
+                            player.TakeDamage();
+                            player.respawn();
+                            bullet.DeactivateBullet();
+                            auto itShooter = std::find_if(players.begin(), players.end(),
+                                [&](const Player& p) { return p.GetPlayerID() == shooterID; });
+                            if (itShooter != players.end()) {
+                                itShooter->AddPoints();
+                            }
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
 bool BulletManager::CanShoot()
 {
      return  (std::chrono::steady_clock::now() - m_lastShotTime) >= GameSettings::COOL_DOWNTIME;
