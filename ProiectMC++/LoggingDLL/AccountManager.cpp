d91573c0255e057d1f82de1fffaa2cac3282a26c
Silviu-Unitbv <ionut.hermeneanu@student.unitbv.ro>
2024-12-29 17:46:20 +0200

    Create AccountManager in dll

---
+++ ProiectMC++/LoggingDLL/AccountManager.cpp
@@ -0,0 +1,45 @@
+
+#include "pch.h"
+#include "AccountManager.h"
+#include <fstream>a
+#include <iostream>
+
+AccountManager::AccountManager() : username(""), password(""), points(0) {}
+
+AccountManager::AccountManager(const std::string& user, const std::string& pass, int pts)
+    : username(user), password(pass), points(pts) {
+}
+
+void AccountManager::setUsername(const std::string& user) { username = user; }
+void AccountManager::setPassword(const std::string& pass) { password = pass; }
+void AccountManager::setPoints(int pts) { points = pts; }
+
+std::string AccountManager::getUsername() const { return username; }
+std::string AccountManager::getPassword() const { return password; }
+int AccountManager::getPoints() const { return points; }
+
+bool AccountManager::authenticate(const std::string& user, const std::string& pass) const {
+    return username == user && password == pass;
+}
+
+void AccountManager::saveDataToFile(const std::string& filename) const {
+    std::ofstream outFile(filename);
+    if (!outFile) {
+        std::cerr << "Unable to open file for saving\n";
+        return;
+    }
+    outFile << username << "\n" << password << "\n" << points << "\n";
+    outFile.close();
+}
+
+void AccountManager::loadDataFromFile(const std::string& filename) {
+    std::ifstream inFile(filename);
+    if (!inFile) {
+        std::cerr << "Unable to open file for loading\n";
+        return;
+    }
+    std::getline(inFile, username);
+    std::getline(inFile, password);
+    inFile >> points;
+    inFile.close();
+}
---
+++ ProiectMC++/LoggingDLL/AccountManager.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <string>
+
+#ifdef ACCOUNTMANAGER_EXPORTS
+#define ACCOUNTMANAGER_API __declspec(dllexport)
+#else
+#define ACCOUNTMANAGER_API __declspec(dllimport)
+#endif
+
+class ACCOUNTMANAGER_API AccountManager {
+private:
+    std::string username;
+    std::string password;
+    int points;
+
+public:
+    AccountManager();
+    AccountManager(const std::string& user, const std::string& pass, int pts = 0);
+
+    void setUsername(const std::string& user);
+    void setPassword(const std::string& pass);
+    void setPoints(int pts);
+
+    std::string getUsername() const;
+    std::string getPassword() const;
+    int getPoints() const;
+
+    bool authenticate(const std::string& user, const std::string& pass) const;
+    void saveDataToFile(const std::string& filename) const;
+    void loadDataFromFile(const std::string& filename);
+};
